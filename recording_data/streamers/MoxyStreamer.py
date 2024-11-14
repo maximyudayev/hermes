9e87ab1643abe6732d50de0104f596aab482ab80
Juha <juha.carlon@kuleuven.be>
2024-11-14 16:44:43 +0100

    (temp) added moxy files

---
+++ recording_data/streamers/MoxyStreamer.py
@@ -0,0 +1,181 @@
+from collections import OrderedDict, defaultdict
+import queue
+
+import numpy as np
+
+
+from streamers.SensorStreamer import SensorStreamer
+from streams.MoxyStream import MoxyStream
+
+from utils.msgpack_utils import serialize
+from utils.print_utils import *
+
+from openant.easy.node import Node
+from openant.devices.common import DeviceType, AntPlusDevice
+from openant.devices.scanner import Scanner
+from openant.devices.utilities import auto_create_device
+from openant.devices import ANTPLUS_NETWORK_KEY
+
+class CustomNode(Node):
+
+    def __init__(self):
+        super().__init__()
+
+    def _main(self):
+        while self._running:
+            try:
+                (data_type, channel, data) = self._datas.get(True, 1.0)
+                self._datas.task_done()
+                if data_type == "broadcast":
+                    byte_data = bytes(data)
+                    if byte_data[0] == 1:
+                        counter = byte_data[1]
+                        payload = int.from_bytes(byte_data[4:8],  byteorder='big', signed=False)
+                        total_hem = (payload >> 20) * 0.01
+                        previous_sat_hem = ((payload % (2 ** 20)) >> 10) * 0.1
+                        current_sat_hem = (payload % (2 ** 10)) * 0.1
+                        id = f"{byte_data[8]}.{byte_data[9]}.{byte_data[10]}.{byte_data[11]}:{byte_data[12]}"
+                        print(f"device: {id}; packet {counter}; total hem : {total_hem}g/dl; previous sat : {previous_sat_hem}%; current sat: {current_sat_hem}%")
+                    self.channels[channel].on_broadcast_data(data)
+                elif data_type == "burst":
+                    self.channels[channel].on_burst_data(data)
+                elif data_type == "broadcast_tx":
+                    self.channels[channel].on_broadcast_tx_data(data)
+                elif data_type == "acknowledge":
+                    self.channels[channel].on_acknowledge_data(data)
+                else:
+                    print("Unknown data type '%s': %r", data_type, data)
+            except queue.Empty as _:
+                pass
+
+    def sample_devices(self):
+        timer = 10
+        start_time = time.time()
+        self.devices = set()
+        while time.time() - timer < start_time:
+            try:
+                (data_type, channel, data) = self._datas.get(True, 1.0)
+                self._datas.task_done()
+                if data_type == "broadcast":
+                    byte_data = bytes(data)
+                    id = f"{byte_data[8]}.{byte_data[9]}.{byte_data[10]}.{byte_data[11]}:{byte_data[12]}"
+                    if id not in self.devices:
+                        print(f"device: {id} found")
+                        self.channels[channel].on_broadcast_data(data)
+                        self.devices.add(id)
+            except queue.Empty as _:
+                pass
+        if len(self.devices) == 3:
+            return True
+        else:
+            return False
+
+
+class MoxyStreamer(SensorStreamer):
+  # Mandatory read-only property of the abstract class.
+  _log_source_tag = "moxy"
+
+  ########################
+  ###### INITIALIZE ######
+  ########################
+
+  def __init__(self, 
+               port_pub: str = None,
+               port_sync: str = None,
+               port_killsig: str = None,
+               print_status: bool = True, 
+               print_debug: bool = False):
+
+    stream_info = {}
+
+    super().__init__(port_pub=port_pub,
+                        port_sync=port_sync,
+                        port_killsig=port_killsig,
+                        stream_info=stream_info,
+                        print_status=print_status, 
+                        print_debug=print_debug)
+    
+
+    self.devices = ["128.69.31.31:5",
+                    "128.68.31.31:5",
+                    "128.67.31.31:5"] # list of device ids
+    
+   
+
+  #def create_stream(argumets) -> AwindaStream:  
+  #  return AwindaStream(argumets["device_mapping"], argumets["num_joints"], argumets["radio_channel"])
+
+  def create_stream(self) -> MoxyStream:
+    return MoxyStream(self.devices)
+  
+  def connect(self) -> None:
+    
+    self.devices = []
+    self.node = CustomNode()
+    self.node.set_network_key(0x00, ANTPLUS_NETWORK_KEY)
+
+    self.scanner = Scanner(self.node, device_id=0, device_type=0)
+
+    def on_update(device_tuple, common):
+        device_id = device_tuple[0]
+        print(f"Device #{device_id} common data update: {common}")
+
+    # local function to call when device update device speific page data
+    def on_device_data(device, page_name, data):
+        print(f"Device: {device}, broadcast: {page_name}, data: {data}")
+
+    # local function to call when a device is found - also does the auto-create if enabled
+    def on_found(device_tuple):
+        print("device found")
+        return
+        device_id, device_type, device_trans = device_tuple
+        print(
+            f"Found new device #{device_id} {DeviceType(device_type)}; device_type: {device_type}, transmission_type: {device_trans}"
+        )
+        if len(self.devices) < 16:
+            try:
+                dev = auto_create_device(self.node, device_id, device_type, device_trans)
+                # closure callback of on_device_data with device
+                dev.on_device_data = lambda _, page_name, data, dev=dev: on_device_data(
+                    dev, page_name, data
+                )
+                self.devices.append(dev)
+            except Exception as e:
+                print(f"Could not auto create device: {e}")
+
+    self.scanner.on_found = on_found
+    self.scanner.on_update = on_update
+
+    self.node.sample_devices()
+
+
+  # Acquire data from the sensors until signalled externally to quit
+  def run(self) -> None:
+    # While background process reads-out new data, can do something useful
+    #   like poll for commands from the Broker to terminate, and block on the Queue 
+    while self._running:
+        counter_per_sensor = defaultdict(lambda: -1)
+        try:
+            (data_type, channel, data) = self.node._datas.get(True, 1.0)
+            self.node._datas.task_done()
+            if data_type == "broadcast":
+                byte_data = bytes(data)
+                if data[0] == 1:
+                    time_s = time.time()
+                    counter = data[1]
+                    THb = ((int(data[4] >> 4) << 4) + (int(data[4] % 2**4)) + (int(data[5] % 2**4) << 8)) * 0.01
+                    SmO2 = ((int(data[7] >> 4) << 6) + (int(data[7] % 2**4) << 2) + int(data[6] % 2**4)) * 0.1
+                    id = f"{byte_data[8]}.{byte_data[9]}.{byte_data[10]}.{byte_data[11]}:{byte_data[12]}"
+                    if counter_per_sensor[id] != counter:
+                        self._stream.append_data(id, time_s, THb, SmO2, counter)
+                    self.node.channels[channel].on_broadcast_data(data)
+            else:
+                  print("Unknown data type '%s': %r", data_type, data)
+        except queue.Empty as _:
+              pass        
+
+  
+  # Clean up and quit
+  def quit(self) -> None:
+    
+    super().quit(self)
---
+++ recording_data/streams/MoxyStream.py
@@ -0,0 +1,79 @@
+from collections import OrderedDict
+import numpy as np
+from streams.Stream import Stream
+from visualizers import LinePlotVisualizer
+
+################################################
+################################################
+# A structure to store Awinda MTws' stream's data.
+################################################
+################################################
+class MoxyStream(Stream):
+  def __init__(self, devices) -> None:
+    super().__init__()
+    
+    self._device_name = 'MoxyStream'
+    
+    # Invert device mapping to map device_id -> joint_name
+
+    self._define_data_notes()
+
+    # When using onLiveDataAvailable, every immediately available packet from each MTw is pushed in its own corresponding Stream.
+    # When using onAllLiveDataAvailable, packets are packaged all at once (potentially for multiple timesteps)
+    #   with interpolation of data for steps where some of sensors missed a measurement.
+    # Choose the desired behavior for the system later. (currently onAllLiveDataAvailable).
+    for dev in devices:
+        self.add_stream(device_name=dev,
+                            stream_name='THb',
+                            data_type='float32',
+                            sample_size=[1],
+                            sampling_rate_hz=0.5,
+                            extra_data_info={},
+                            data_notes="")
+            
+        self.add_stream(device_name=dev,
+                            stream_name='SmO2',
+                            data_type='float32',
+                            sample_size=[1],
+                            sampling_rate_hz=0.5,
+                            extra_data_info={},
+                            data_notes="")
+            
+        self.add_stream(device_name=dev,
+                            stream_name='counter',
+                            data_type='uint8',
+                            sample_size=[1],
+                            sampling_rate_hz=0.5,
+                            extra_data_info={},
+                            data_notes="")
+
+  def append_data(self,
+                  device_id: int,
+                  time_s: float,
+                  THb: np.ndarray,
+                  SmO2: np.ndarray,
+                  counter: np.ndarray):
+    self._append_data(device_id, 'THb', time_s, THb)
+    self._append_data(device_id, 'SmO2', time_s, SmO2)
+    self._append_data(device_id, 'counter', time_s, counter)
+
+
+
+  ###########################
+  ###### VISUALIZATION ######
+  ###########################
+
+  # Specify how the streams should be visualized.
+  # Return a dict of the form options[device_name][stream_name] = stream_options
+  #  Where stream_options is a dict with the following keys:
+  #   'class': A subclass of Visualizer that should be used for the specified stream.
+  #   Any other options that can be passed to the chosen class.
+  def get_default_visualization_options(self):
+    visualization_options = {}
+
+    visualization_options[self._device_name] = {}
+
+    return visualization_options
+  
+  def _define_data_notes(self):
+    self._data_notes = {}
