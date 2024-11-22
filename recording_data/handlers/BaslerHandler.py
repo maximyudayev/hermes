47d6436ebe79b7761079010658475f2110645dd6
Maxim Yudayev <maxim.yudayev@gmail.com>
2024-11-22 17:22:05 +0100

    feat(basler): offload ZeroMQ logic from SDK thread

--- recording_data/handlers/BaslerHandler.py
+++ recording_data/handlers/BaslerHandler.py
@@ -1,18 +1,20 @@
-from typing import Callable
+from collections import OrderedDict
+import queue
+from threading import Lock
+from typing import Callable, Generator
 from pypylon import pylon
 import numpy as np
 
 from utils.print_utils import *
 
 class ImageEventHandler(pylon.ImageEventHandler):
-  def __init__(self, cam_array):
+  def __init__(self, cam_array, buffer_size=5):
     super().__init__()
     self._cam_array = cam_array
     # Register with the pylon loop
     self._cam_array.RegisterImageEventHandler(self, pylon.RegistrationMode_ReplaceAll, pylon.Cleanup_None)
-
-  def _callback(self, camera_id: str, frame: np.ndarray, timestamp: np.uint64, sequence_id: np.int64) -> None:
-    pass
+    self._buffers = OrderedDict([(cam.GetDeviceInfo().GetSerialNumber(), queue.Queue(maxsize=buffer_size)) for cam in cam_array])
+    self._lock = Lock()
 
   def OnImageGrabbed(self, camera, res: pylon.GrabResultData):
     # Gets called on every image.
@@ -26,7 +28,7 @@ class ImageEventHandler(pylon.ImageEventHandler):
         camera_id: str = camera.GetDeviceInfo().GetSerialNumber()
         timestamp: np.uint64 = res.GetTimeStamp()
         sequence_id: np.int64 = res.GetImageNumber()
-        self._callback(camera_id=camera_id, frame=frame, timestamp=timestamp, sequence_id=sequence_id)
+        self._put_frame(camera_id=camera_id, frame=frame, timestamp=timestamp, sequence_id=sequence_id)
       else:
         raise RuntimeError("Grab Failed")
     except Exception as e:
@@ -34,5 +36,13 @@ class ImageEventHandler(pylon.ImageEventHandler):
 
   def OnImagesSkipped(self, camera, countOfSkippedImages):
     msg = log_debug(f"{camera.GetDeviceInfo().GetSerialNumber()} skipped {countOfSkippedImages} images.")
+    print(msg)
 
-  
+  def _put_frame(self, camera_id: str, frame: np.ndarray, timestamp: np.uint64, sequence_id: np.int64) -> None:
+    self._buffers[camera_id].put()
+
+  def get_frame() -> Generator[tuple[str, np.ndarray, np.uint64, np.int64]]:
+    yield
+
+  def is_data_available() -> bool:
+    pass
