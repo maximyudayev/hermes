0a34a7fecd169e61d24d73475d4e0e75551eaa80
Maxim Yudayev <maxim.yudayev@gmail.com>
2025-02-10 01:11:55 +0100

    chore: pragmas/configs extended

--- recording_data/utils/zmq_utils.py
+++ recording_data/utils/zmq_utils.py
@@ -1,14 +1,20 @@
+# ZeroMQ topics and message strings
 TOPIC_KILL = 'KILL'
-
 CMD_GO = 'GO'
 CMD_END = 'END'
 CMD_BYE = 'BYE'
 
+# Ports used for ZeroMQ by our system
 PORT_BACKEND = '42069'
 PORT_FRONTEND = '42070'
 PORT_SYNC = '42071'
 PORT_KILL ='42066'
 
+# Ports of connected devices/sensors
+PORT_MOTICON ='8888' # defined by the Moticon desktop app, putting data at the loopback address for listening
+PORT_PROSTHESIS ='51267' # defined by LabView code of VUB
+
+# IP addresses of devices on the network used by our system
 IP_LOOPBACK = '127.0.0.1'
 DNS_LOCALHOST = 'localhost'
 IP_STATION = '192.168.69.100'
